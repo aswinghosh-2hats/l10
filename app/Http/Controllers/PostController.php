6f89e04226ca0765d35d3694176db43fa7aa4341
Aswin Ghosh <aswin@2hatslogic.com>
2023-10-09 12:21:03 +0530

    test: add complexity test for codacy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -51,6 +51,27 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $condition1 = true;
+        $condition2 = false;
+        $condition3 = true;
+        $condition4 = false;
+
+        if ($condition1) {
+            if ($condition2 || (!$condition3 && $condition4)) {
+                if ($condition3 && $condition4 || ($condition1 && !$condition2)) {
+                    echo "Condition 1 is true, Condition 2 is true or Condition 3 and 4 are false, and either Condition 3 and 4 are true or Condition 1 is true and Condition 2 is false.";
+                } else {
+                    if ($condition2 && $condition3 || (!$condition4 && $condition1)) {
+                        echo "Nested else statements make code hard to read, and this is a convoluted mess.";
+                    } else {
+                        echo "I hope you're not trying to make sense of this!";
+                    }
+                }
+            } else {
+                echo "This is a prime example of how not to structure your code.";
+            }
+        } else {
+            echo "The outermost if condition is false.";
+        }
     }
 }

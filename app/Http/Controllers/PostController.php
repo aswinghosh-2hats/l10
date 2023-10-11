a16ed47e018372d1069286b253285d679997fcfb
Aswin Ghosh <aswin@2hatslogic.com>
2023-10-11 12:58:33 +0530

    test/codium

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,71 +9,129 @@ use App\Models\User;
 
 class PostController extends Controller
 {
-    public function index()
-{
-    $userData = User::get(); // Fetch all users from the database
+    public function unformattedCode()
+    {
+        $userData = User::get(); // Fetch all users from the database
+        $userNum = '';
+        $userStr = '';
+        foreach ($userData as $usr)
+        {
+        $userId = $usr->user_id; // Get the user ID
 
-    foreach ($userData as $usr) {
-    $userId = $usr->user_id; // Get the user ID
+            for ($i = 0; $i < strlen($userId); $i++) {
+                $char = $userId[$i]; // Get each character of the user ID
 
-        for ($i = 0; $i < strlen($userId); $i++) {
-            $char = $userId[$i]; // Get each character of the user ID
+                if (ctype_digit($char)) {
+                    $userNum .= $char; // Concatenate digits to form a number
+                } else {
+                    $userStr .= $char; // Concatenate non-digit characters to form a string
+                }
+            }
 
-            if (ctype_digit($char)) {
-                $userNum .= $char; // Concatenate digits to form a number
+            // Perform some arbitrary and unnecessary logic
+            if ($i % 2 == 0) {
+                $result = $this->processUser($userNum, $userStr);
+                echo "User processed: " . $result . "<br>";
             } else {
-                $userStr .= $char; // Concatenate non-digit characters to form a string
+                echo "Skipping user with ID: " . $userId . "<br>";
             }
         }
-
-        // Perform some arbitrary and unnecessary logic
-        if ($i % 2 == 0) {
-            $result = $this->processUser($userNum, $userStr);
-            echo "User processed: " . $result . "<br>";
-        } else {
-            echo "Skipping user with ID: " . $userId . "<br>";
-        }
     }
-}
 
-private function processUser($num, $str)
-{
-    $num *= 2; // Double the numeric part of the user ID
-    $str = strtoupper($str); // Convert the string part to uppercase
+    private function buggyCode($num, $str): bool
+    {
+        $a = 0;
 
-    return $num . $str; // Combine the modified numeric and string parts
-}
+        for ($i = 0; $i < strlen($num); $i++) {
+            $a += $num[$i];
+        }
 
+        return $x;
+    }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function invalidReturnType(Post $post): bool
     {
-        //
+        $variable = 'x';
+
+        if (strlen($variable) > 0) {
+            return $variable;
+        }
+
+        return 'false';
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function possiblyInvalidReturnType(Post $post): bool
+    {
+        $variable = 'x';
+
+        if (strlen($variable) > 0) {
+            return $variable;
+        }
+
+        return false;
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Post $post)
+    public function unsecureCode()
     {
-        //
+        $password = 'sdkjflkidsjlgfd';
+        $key = 'sddsfcdsf_35345_gtrt';
+
+        return [
+            'password' => $password,
+            'key' => $key
+        ];
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function complex()
     {
-        //
+        $k=100;
+
+        if ($k > 50) {
+            if ($k > 75) {
+                if ($k > 100) {
+                    if ($k > 125) {
+                        if ($k > 150) {
+                            if ($k > 175) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        } elseif ($k > 25) {
+            if ($k > 75) {
+                if ($k > 100) {
+                    if ($k > 125) {
+                        if ($k > 150) {
+                            if ($k > 175) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            return false;
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function unsecureShell($cmd)
     {
-        //
+        shell_exec($cmd);
     }
 }
